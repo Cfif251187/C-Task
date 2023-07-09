f1708bc02160cef12c9977aa858c6fdeaba76fde
Sasha <efremovasasha@rambler.ru>
2023-07-09 20:08:46 +0300

    .\1\

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №2. Программа принимает два числа и выдает большее.
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
