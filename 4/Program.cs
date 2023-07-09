00ef0ecb60c8a5c023596a44e01f8d922e375b69
Sasha <efremovasasha@rambler.ru>
2023-07-09 20:16:29 +0300

    .\4\

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 8. Программа показывает все четные числа от 1 до введенного числа.
+Console.Write("Input posiniv number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while(current <= number )
+{
+    Console.WriteLine(current +" ");
+     current = current +2;
+}
