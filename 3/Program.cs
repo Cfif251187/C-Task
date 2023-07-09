a06396f3f7905e28ecf598876f4b6c3f5cf1d794
Sasha <efremovasasha@rambler.ru>
2023-07-09 20:14:05 +0300

    .\3\

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// // Задача 6. Программа определяет является ли число четным
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num  % 2) == 0)
+{
+    Console. WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Uneven number");
+}
