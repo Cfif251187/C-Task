459c65a58e720aaba42cc8ece821836877ca121a
Sasha <efremovasasha@rambler.ru>
2023-07-09 20:11:39 +0300

    .\2\

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4. Программа выдает максимальное из трех чисел.
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
